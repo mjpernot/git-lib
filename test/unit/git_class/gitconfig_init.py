2fd1fc3385e2f06ac8d2cf10ba53193e817bde5e
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-06-13 14:44:20 +0100

    Add test/unit/git_class/gitconfig_init.py

---
+++ test/unit/git_class/gitconfig_init.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  gitconfig_init.py
+
+    Description:  Unit testing of gitconfig.__init__ in git_class.py.
+
+    Usage:
+        test/unit/git_class/gitconfig_init.py
+
+    Arguments:
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import sys
+import os
+import shutil
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+# Third-party
+import mock
+
+# Local
+sys.path.append(os.getcwd())
+import git_class
+import lib.gen_libs as gen_libs
+import version
+
+__version__ = version.__version__
+
+
+class UnitTest(unittest.TestCase):
+
+    """Class:  UnitTest
+
+    Description:  Class which is a representation of a unit testing.
+
+    Super-Class:  unittest.TestCase
+
+    Sub-Classes:
+
+    Methods:
+        setUp -> Unit testing initilization.
+        test_init_default -> Test with default values set.
+        tearDown -> Clean up of unit testing.
+
+    """
+
+    def setUp(self):
+
+        """Function:  setUp
+
+        Description:  Initialization for unit testing.
+
+        Arguments:
+
+        """
+
+        self.repo_dir = "test/unit/git_class/test/test-repo"
+
+    def test_init_default(self):
+
+        """Function:  test_init_default
+
+        Description:  Test with default values set.
+
+        Arguments:
+
+        """
+
+        gitr = git_class.GitConfig(self.repo_dir)
+
+        self.assertEqual(gitr.repo_dir, self.repo_dir)
+
+    def tearDown(self):
+
+        """Function:  tearDown
+
+        Description:  Clean up of unit testing.
+
+        Arguments:
+
+        """
+
+        if os.path.isdir(self.repo_dir):
+            shutil.rmtree(self.repo_dir)
+
+
+if __name__ == "__main__":
+    unittest.main()
