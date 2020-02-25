9ce9ae20c7b96aaad4fc9207bb06477ce9bdc2be
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-02-25 16:22:15 +0000

    Update test/unit/sonarqube_code_coverage.sh

--- test/unit/sonarqube_code_coverage.sh
+++ test/unit/sonarqube_code_coverage.sh
@@ -11,6 +11,7 @@ echo "Running unit test modules in conjunction with coverage"
 coverage run -a --source=git_class test/unit/git_class/gitmerge_init.py
 coverage run -a --source=git_class test/unit/git_class/gitmerge_detach_head.py
 coverage run -a --source=git_class test/unit/git_class/gitmerge_get_br_name.py
+coverage run -a --source=git_class test/unit/git_class/gitmerge_remove_branch.py
 coverage run -a --source=git_class test/unit/git_class/gitmerge_create_gitrepo.py
 coverage run -a --source=git_class test/unit/git_class/gitmerge_set_remote.py
 coverage run -a --source=git_class test/unit/git_class/gitmerge_is_remote.py
